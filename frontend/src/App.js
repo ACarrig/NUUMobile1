e2f17d39f36de2fcbd90c7aafb62350f77c10c2d
Ik Teng Liaw <iktengliaw@gmail.com>
2025-02-24 15:57:38 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import FileUpload from "./Components/FileUpload";
-import Footer from "./Components/Footer";
 import Analysis from "./DataBreakdown/Analysis"; // Import Analysis component
-import "./style.css"; // Importing the CSS file
 import FileUpload from './components/FileUpload';  // Import your upload page component
 import Navbar from './components/Navbar';  // Import your Navbar component
 
@@ -16,7 +12,6 @@ const App = () => {
         <Route path="/" element={<FileUpload />} />
         <Route path="/analysis" element={<Analysis />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
