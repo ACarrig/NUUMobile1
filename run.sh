357974eed1bf65ed10ccc0fd35dc16f78b4e3bae
Ik Teng Liaw <iktengliaw@gmail.com>
2025-03-09 22:06:04 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -53,6 +53,7 @@ fi
 
 # Build and start the docker image
 echo "Fetch and build docker image"
+docker rm -f ollama
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 docker exec -it ollama ollama pull llama3.2
 
